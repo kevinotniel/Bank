4aeb68c185f254b2283fd4aa18486f64972f4164
DESKTOP-PL7NIVF\ACER <kevinotniel3@gmail.com>
2021-04-29 12:06:36 +0700

    acc

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Kevin Otniel',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Moci Momo',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Bruno Ann',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Milo Rap',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -104,7 +104,7 @@ const displayMovements = function (acc, sort = false) {
 
   const movs = sort ? acc.slice().sort((a, b) => a - b) : movements;
 
-  acc.forEach(function (mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -223,7 +223,7 @@ const calcDisplaySummary = function (acc) {
 // Event Handler
 
 const updateUI = function (acc) {
-  displayMovements(acc);
+  displayMovements(acc.movements);
 
   balanceDisplayMovements(acc);
 
@@ -272,10 +272,9 @@ btnTransfer.addEventListener('click', function (e) {
     receiveAcc?.username !== currentAccount.username
   ) {
     currentAccount.movements.push(-amount);
-    receiveAcc.movements.push(amount);
-
-    updateUI(currentAccount);
-  }
+    receiveAcc.movements.push(amount); 
+   }
+   updateUI(currentAccount);
 });
 
 btnLoan.addEventListener('click', function (e) {
